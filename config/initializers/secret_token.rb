eeaeda0c2f87d650d1e6cac5dd93c3b9683bb433
Kristian <kurisu@iki.fi>
2014-02-27 15:26:48 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tabora::Application.config.secret_key_base = '39be1c66a3054fc53d9cfd9ee2f36a69e03fe6ec83c6ce517187d87d6284ade2ef6c3aadac11cf1bb897856dfccc8641c08ba45bab61176c6cd710dc62cc148c'
