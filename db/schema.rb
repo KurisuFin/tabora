aee09cea13cb0c646dd01b06e7e30636631a0bce
Kristian <kurisu@iki.fi>
2014-02-28 16:09:04 +0200

    name added to Event

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140228133826) do
+ActiveRecord::Schema.define(version: 20140228135020) do
 
   create_table "events", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it 'is saved with name' do
+		event = Event.create name:'Happening'
+
+		expect(event).to be_valid
+		expect(Event.count).to eq(1)
+	end
+
+	it 'is not saved without name' do
+		event = Event.create name:''
+
+		expect(event).not_to be_valid
+		expect(Event.count).to eq(0)
+	end
+
+	it 'has to have unique name' do
+		Event.create name:'Happening'
+		event = Event.create name:'Happening'
+
+		expect(event).not_to be_valid
+		expect(Event.count).to eq(1)
+	end
 end
