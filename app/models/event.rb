3b3e88a12c4d8f03ddd30e484bcf3559c4f60dfb
KurisuFin <kristian.pulkkinen@cs.helsinki.fi>
2014-03-10 14:15:16 +0200

    operator edit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true
 
+	has_many :operators
+	has_many :operating_user, through: :operators, source: :users
 	has_many :tournaments, dependent: :destroy
 end
---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+	has_many :operators
+	has_many :operated_events, through: :operators, source: :events
+	has_many :participations
+	has_many :tournaments, through: :participations
+
+	validates :username,
+						uniqueness: true,
+						length: { minimum: 3, maximum: 16 }
+
+	#TODO Password things
+	def self.authenticate(username)
+		User.find_by username:username
+	end
+
+	def is_participating_of(tournament)
+		Tournament.find(tournament).participants.exists? self
+	end
+
+	def is_operator_of(event)
+		Event.find(event).operators.exists? self
+	end
+end
